9af1eba93789c5138c3567c284674554afa79d0e
Nhhonho <Vivian508933@gmail.com>
2023-12-16 21:56:25 +0800

    2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 import psycopg2
 import csv
 from io import TextIOWrapper
@@ -13,6 +13,15 @@ DATABASE_CONFIG = {
     'password': 'your_password'
 }
 
+def is_location_name_duplicate(cursor, location_name):
+    # Check if a location with the same name already exists in the database
+    cursor.execute("SELECT COUNT(*) FROM VACCINATION_LOCATION WHERE Location_name = %s", (location_name,))
+    return cursor.fetchone()[0] > 0
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/upload_csv', methods=['POST'])
 def upload_csv():
     try:
@@ -20,20 +29,28 @@ def upload_csv():
         conn = psycopg2.connect(**DATABASE_CONFIG)
         cursor = conn.cursor()
 
-        # Get the uploaded CSV file from the request
+        # Get the uploaded CSV file
         csv_file = request.files['csvFile']
         csv_reader = csv.reader(TextIOWrapper(csv_file))
 
-        # Begin the transaction
-        conn.execute("BEGIN")
 
         # Insert CSV data into the database
         for row in csv_reader:
-            cursor.execute("INSERT INTO csv_data (column1, column2) VALUES (%s, %s)", (row[0], row[1]))
-            # Add more columns and placeholders as needed
+            location_id, location_name, location_address, slot_capacity, service_start_time, service_end_time = row
+
+            # Check if the location name already exists
+            if is_location_name_duplicate(cursor, location_name):
+                conn.rollback()
+                raise ValueError(f"Location with name '{location_name}' already exists in the database")
+
+            cursor.execute(
+                "INSERT INTO VACCINATION_LOCATION (Location_id, Location_name, Location_address, Slot_capacity, Service_start_time, Service_end_time) "
+                "VALUES (%s, %s, %s, %s, %s, %s)",
+                (location_id, location_name, location_address, int(slot_capacity), service_start_time, service_end_time)
+            )
 
         # Commit the transaction if all operations are successful
-        conn.execute("COMMIT")
+        conn.commit()
 
         # Close the database connection
         conn.close()
@@ -42,7 +59,7 @@ def upload_csv():
 
     except Exception as e:
         # Rollback the transaction if any error occurs
-        conn.execute("ROLLBACK")
+        conn.rollback()
         print(f"Transaction rolled back due to error: {str(e)}")
         return jsonify({"status": "error", "message": str(e)})
 

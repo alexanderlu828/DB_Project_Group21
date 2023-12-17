340a6edfa5a1a0d3d1f1f1aedeece40aef346998
Nhhonho <73788814+Nhhonho@users.noreply.github.com>
2023-12-18 02:08:11 +0800

    Update user.php

--- user.php
+++ user.php
@@ -18,8 +18,9 @@
         }
     ?>
 
+    <!-- 查醫院存量，當初是想說是選個時段，只要那時間之後有位置都會顯示，也可以改 -->
+    <!-- 目前有對應到醫院選項，但我的電腦好像顯示不出中文...？ -->
     <h2>查詢醫院存量：</h2>
-
     <div class="user-container">
     <form id="searchForm" action="">
     <label for="hospital">Hospital:</label>
@@ -45,20 +46,19 @@
 
     <label for="timeSlot">時段:</label>
     <select id="time" name="time">
-        <!-- JavaScript 生成時段選項 -->
+        <!-- 下面JavaScript 生成時段選項 -->
     </select><br><br>
 
 
     <script>
+        // JavaScript 生成時段選項
         const hospitalSelect = document.getElementById('hospital');
         const timeSelect = document.getElementById('time');
 
         hospitalSelect.addEventListener('change', () => {
             const selectedHospital = hospitalSelect.value;
 
-            // 使用 Ajax 或者在後端 PHP 中根據選擇的醫院名稱查詢時間範圍，然後動態生成選項？
-            // 這裡我先用範例來動態生成選項
-            // 需要寫一個後端的 PHP 腳本來回傳對應醫院的時間範圍
+            // 需要寫一個後端的 PHP 腳本來回傳對應醫院的時間範圍，我目前是直接列出選項
             const fakeTimeData = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00']; // 假設的時間範圍，實際應由後端提供
 
             // 清空原先的選項
@@ -78,23 +78,24 @@
     <p id="slotCapacityResult"></p>
     <div id="slotCapacityResult"></div>
     
-
+    
     <script>
-    function fetchSlotCapacity() {
-        const selectedHospital = hospitalSelect.value; // 從前端取得用戶選擇的醫院名稱
-        const selectedTime = timeSelect.value; // 從前端取得用戶選擇的時間範圍
+        // 上面按鈕的javascript
+        function fetchSlotCapacity() {
+            const selectedHospital = hospitalSelect.value; // 從前端取得用戶選擇的醫院名稱
+            const selectedTime = timeSelect.value; // 從前端取得用戶選擇的時間範圍
 
-        // 使用 fetch 函數向後端發送請求
-        fetch(`your_backend_script.php?hospital=${selectedHospital}&time=${selectedTime}`)
-            .then(response => response.json())
-            .then(data => {
-                const slotCapacityResult = document.getElementById('slotCapacityResult');
-                slotCapacityResult.textContent = `Slot Capacity: ${data.slot_capacity}`;
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-    }
+            // 使用 fetch 函數向後端發送請求，這是模版，如果要用，your_backend_script.php記得要改！
+            fetch(`your_backend_script.php?hospital=${selectedHospital}&time=${selectedTime}`)
+                .then(response => response.json())
+                .then(data => {
+                    const slotCapacityResult = document.getElementById('slotCapacityResult');
+                    slotCapacityResult.textContent = `Slot Capacity: ${data.slot_capacity}`;
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+        }
     </script></div><br><br><br><br><br>
  
 
@@ -126,28 +127,31 @@
         </form>
     </div>
     <script>
-    function saveReservation() {
-        // 預約時輸入資料，可能要改一下名字
-        var name = document.getElementById('name').value;
-        var name = document.getElementById('health_card_number').value;
-        var gender = document.getElementById('gender').value;
-        var phone = document.getElementById('phone').value;
-        var birthday = document.getElementById('birthday').value; 
-        var address = document.getElementById('address').value; 
+        //按鈕的javascript，按下後要上傳到後端
+        function saveReservation() {
+            // 預約時輸入資料，可能要改一下對應的後端名字
+            var name = document.getElementById('name').value;
+            var name = document.getElementById('health_card_number').value;
+            var gender = document.getElementById('gender').value;
+            var phone = document.getElementById('phone').value;
+            var birthday = document.getElementById('birthday').value; 
+            var address = document.getElementById('address').value; 
 
-        console.log("Name: " + name);
-        console.log("health_card_number: " + name);
-        console.log("Gender: " + gender);
-        console.log("Phone: " + phone);
-        console.log("Birthday: " + birthday); 
-        console.log("Address: " + address); 
+            console.log("Name: " + name);
+            console.log("health_card_number: " + name);
+            console.log("Gender: " + gender);
+            console.log("Phone: " + phone);
+            console.log("Birthday: " + birthday); 
+            console.log("Address: " + address); 
     }
-
     </script><br><br><br><br><br>
 
 
-    <h2>預約記錄：</h2>
 
+
+    <!-- 這裡我想了一下，應該可以用user_id查詢記錄(我在admin2有寫)，只是後面回報副作用跟feedback要對應user_id上傳-->
+    <!-- 目前可以查到user資料，後面對應feedback跟回饋還沒動-->
+    <h2>預約記錄：</h2>
     <div class="user-container">
         <form id="appointmentForm" action="">
             <label for="userId">User ID:</label>
@@ -155,10 +159,17 @@
             <input type="button" value="確定" onclick="getUserAppointment()">
         </form>
 
-        <div id="appointmentResult"></div>
+        <div id="appointmentResult"></div><br><br><br>
+
+        <input type="text" placeholder="在這裡輸入您的feedback...">
+        <input type="button" value="feedback">
+        
+        <input type="text" placeholder="在這裡輸入您的副作用...">
+        <input type="button" value="回報副作用">  
     </div>
 
     <script>
+        //這裡是查後端紀錄的程式碼
         function getUserAppointment() {
             const userId = document.getElementById('userId').value;
             const appointmentResult = document.getElementById('appointmentResult');
@@ -180,7 +191,4 @@
         }
     </script>
 </script>
-<input type="button" value="feedback">
-<input type="button" value="回報副作用">
 
-<script>

5336e1015e1b782ef13e170802024d9fece7ed33
yan0422de <153173336+yan0422de@users.noreply.github.com>
2023-12-18 20:35:44 +0800

    backend - delete location

--- deleteLocation.php
+++ deleteLocation.php
@@ -6,7 +6,7 @@ $dbname = 'postgres';
 $user = 'postgres';
 $password = trim(file_get_contents('db_password.txt'));
 
-$pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+$pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password); 
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $location_id = $_POST['locationId'];
